d9d4d1dafab001264fd91e1301cb04ee1f35b437
CahidArda <cahidardaooz@hotmail.com>
2021-04-16 14:54:14 +0300

    Added Heurisitc 5 and Refactored Heuristic Creation
    Removed create_map, create_new_dict_heu3, result functions.

--- api/new_queries.py
+++ api/new_queries.py
@@ -81,64 +81,47 @@ class QueryGraph:
         role_list = [x['rolename'] for x in graph.run(query).data()]
         return role_list
 
-
+    #Query for Heuristics 1, 2 and 3
     def get_actions_by_role(graph, role_list):
-        role_action = []
+        role_actions = []
         for role in role_list:
             query = '''
                         MATCH (av:Verb)<-[:ACT_VERB]-(i:Id)-[:ROLE]->(r:Role {role_name: {role_var}})
                         with i,av,r
                         match (i:Id)-[:ACT_OBJ]->(ao:Object)
-                        RETURN r.role_name as role, av.action_verb as action_verb, ao.action_object as action_object
+                        RETURN av.action_verb as action_verb, ao.action_object as action_object
                     '''
-            action_object_list = [x['action_verb']+ ' ' +x['action_object'] for x in graph.run(query, role_var=role).data()]
-            role_action.append(action_object_list)
-        return json.loads((str(json.dumps(role_action))))  
+            result = graph.run(query, role_var=role).data()
+            role_actions.append(result)
+        return json.loads((str(json.dumps(role_actions))))  
     
-    
-    def create_map(roles,actions):
-        mapped_val = dict(zip(roles,actions))
-        return json.loads(str(json.dumps(mapped_val)))
-
-
     def create_heuristic1(role_action_map):
-        deflistverb = []
-        for each in list(role_action_map.values()):
+        new_map = defaultdict(list)
+        for role, rows in role_action_map.items():
             d = defaultdict(list)
-            for item in each:
-                d[item.split(' ')[0] + ' operations conducted'].append(item)
-            deflistverb.append(d)
-        return deflistverb
-    
+            for row in rows:
+                d[row['action_verb'] + ' operations conducted'].append(" ".join([row[k] for k in row]))
+            new_map[role] = d
+        return new_map
     
     def create_heuristic2(role_action_map):
-        deflistobj = []
-        for each in list(role_action_map.values()):
+        new_map = defaultdict(list)
+        for role, rows in role_action_map.items():
             d = defaultdict(list)
-            for item in each:
-                d[item.split(' ')[-1] + ' operations done'].append(item)
-            deflistobj.append(d)
-        return deflistobj
-
-    def create_new_dict_heu3(role_action_map):
-        res=[]
-        new_dict = {}
-        for k,v in role_action_map.items():
-            for each in v:
-                res.append(each + ' by ' + k)    
-        new_dict["null"] = res  
-        return new_dict
+            for row in rows:
+                d[row['action_object'] + ' operations done'].append(" ".join([row[k] for k in row]))
+            new_map[role] = d
+        return new_map
 
     def create_heuristic3(role_action_map):
-        new_dict = QueryGraph.create_new_dict_heu3(role_action_map)
-        deflist_h3 = []
-        for each in list(new_dict.values()):
-            d = defaultdict(list)
-            for item in each:
-                d[re.search('s*([^,\.]*?)\s*(,|\.|by)', item).group(1).split(' ', 1)[1]].append(item)
-            deflist_h3.append(d)
-        return deflist_h3
+        container = defaultdict(list)
+        for role, rows in role_action_map.items():
+            for row in rows:
+                container[row['action_object']].append(" ".join([row[k] for k in row]) + " by " + role)
+        new_map = {"null":container}
+        return new_map
 
+    #Query for Heuristics 4 and 5
     def get_actions_and_benefits_by_role(graph, role_list):
         role_actions_benefits = []
         for role in role_list:
@@ -148,28 +131,37 @@ class QueryGraph:
                     match (i:Id)-[:BNF_VERB]->(bv:BenefitVerb)
                     match (i:Id)-[:ACT_OBJ]->(ao:Object)
                     match (i:Id)-[:ACT_VERB]->(av:Verb)
-                    RETURN r.role_name as role, av.action_verb as action_verb, ao.action_object as action_object, bo.benefit_obj as benefit_object, bv.benefit_verb as benefit_verb
+                    RETURN av.action_verb as action_verb, ao.action_object as action_object, bo.benefit_obj as benefit_object, bv.benefit_verb as benefit_verb
                     '''
             result = graph.run(query, role_var=role).data()
-            d = defaultdict(list)
-            for row in result:
-                key = row['benefit_verb'] + " " + row['benefit_object']
-                value = row['action_verb'] + " " + row['action_object']
-                d[key].append(value)
-            role_actions_benefits.append(d)
+            role_actions_benefits.append(result)
         return json.loads((str(json.dumps(role_actions_benefits))))
 
-    def create_heuristic4(role_benefit_map):
-        pass
-    
-    def result(roles,deflist,heuristic_number):
-        if heuristic_number == 'h3':
-            no_actor = ["null"]
-            mapped_val = dict(zip(no_actor, deflist))
-        else:    
-            mapped_val = dict(zip(roles,deflist))
-        return json.loads(str(json.dumps(mapped_val)))
+    def create_heuristic4(role_actions_benefits_map):
+        new_map = defaultdict(list)
+        for role, rows in role_actions_benefits_map.items():
+            d = defaultdict(list)
+            for row in rows:
+                key = "to " + row['benefit_verb'] + " " + row['benefit_object']
+                value = row['action_verb'] + " " + row['action_object']
+                d[key].append(value)
+            new_map[role] = d
+        return new_map
+        
+    def create_heuristic5(role_actions_benefits_map):
+        container = defaultdict(list)
+        for role, rows in role_actions_benefits_map.items():
+            for row in rows:
+                key = "to " + row['benefit_verb'] + " " + row['benefit_object']
+                value = row['action_verb'] + " " + row['action_object']
+                container[key].append(value)
 
+        new_map = {"null": container}
+        return new_map
+    
+    def create_map(roles, objects):
+        mapped_val = dict(zip(roles, objects))
+        return json.loads(str(json.dumps(mapped_val)))
     
     def json_formatter(rslt):
         json_dict = {}
@@ -180,7 +172,6 @@ class QueryGraph:
             json_dict[k]=arr 
         return json_dict    
     
-
     def main(csv_path, heuristic_number):
         graph = Graph(ip_addr = "http://localhost:7474/db/data")
         graph = QueryGraph.delete_all_db(graph)
@@ -200,7 +191,11 @@ class QueryGraph:
                 rslt = QueryGraph.create_heuristic3(data)
 
         elif heuristic_number in ['h4', 'h5']:
-            rslt = QueryGraph.get_actions_and_benefits_by_role(graph,role_list=roles)
+            actions_and_benefits = QueryGraph.get_actions_and_benefits_by_role(graph,role_list=roles)
+            data = QueryGraph.create_map(roles,actions_and_benefits)
+            if heuristic_number == 'h4':
+                rslt = QueryGraph.create_heuristic4(data)
+            else:
+                rslt = QueryGraph.create_heuristic5(data)
 
-        solution = QueryGraph.result(roles,rslt, heuristic_number)
-        return json.dumps(QueryGraph.json_formatter(solution))
+        return json.dumps(QueryGraph.json_formatter(rslt))
